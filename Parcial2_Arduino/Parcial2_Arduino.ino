5ad6a48d19f431423b1ba96754f0097f407d09b7
SteveEs25 <nalfaro090@gmail.com>
2018-04-16 21:23:01 -0600

    Terminando Codigo Arduino

--- Parcial2_Arduino/Parcial2_Arduino.ino
+++ Parcial2_Arduino/Parcial2_Arduino.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+int boton = 2;
+int alarma = 9;
 
+void setup() {
+  pinMode(boton, INPUT_PULLUP);
+  pinMode(alarma, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(digitalRead(boton) == 0) {
+    Serial.println("Boton Presionado");
+    delay(500);
+  }
+  if(Serial.available()) {
+    char letra = Serial.read();
+    
+    if(letra == 'H') {
+      analogWrite(alarma, 128);
+    }
+    else if(letra == 'L') {
+      digitalWrite(alarma, LOW);
+    }
+  }
 }
